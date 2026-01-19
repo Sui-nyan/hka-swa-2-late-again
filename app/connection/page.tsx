1891a20ca3345982f92e7897030265b8e31c0313
sui-nyan <78491387+Sui-nyan@users.noreply.github.com>
2026-01-19 08:15:22 +0100

    bleh

--- app/connection/page.tsx
+++ app/connection/page.tsx
@@ -1,10 +1,17 @@
-import { title } from "@/components/primitives";
-import {BasicMap} from "@/app/map/components/BasicMap";
+"use client";
+import {title} from "@/components/primitives";
+import ConnectionCard from "@/app/connection/components/ConnectionCard";
 
 export default function ConnectionPage() {
-  return (
-    <section>
-        <h1 className={title()}>Check Connection</h1>
-    </section>
-  );
+    const connectionData = require('./connection-mock-data.json');
+
+    return (
+        <section className="flex w-full flex-col items-center justify-center gap-4">
+            <h1 className={title()}>Verbindung</h1>
+            {connectionData.connections.map((connection: any, index: number) => (
+            <ConnectionCard key={index} startStation={connection.startStation} endStation={connection.endStation} />
+            ))}
+
+        </section>
+    );
 }
